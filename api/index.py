b98ce05b22dba96bff154c2ff34a578906a9be89
karhin20 <kelvinarhin25@gmail.com>
2025-12-27 23:32:46 +0000

    super admin , multistore

--- api/index.py
+++ api/index.py
@@ -1,10 +1,3 @@
-import sys
-import os
-
-# Add the root directory to the path so that 'app' can be imported
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app.main import app
 
-# This is required for Vercel to find the FastAPI instance
-handler = app
+# Vercel will automatically find the 'app' variable and use it as the ASGI handler.
